f5016536a67cf2e31b40e8af7c04d7e0052a29cd
Ronald <ronaldwilsonlainardi@gmail.com>
2022-04-01 14:14:05 +0700

    making code easier to read

--- src/test/java/steps/Steps.java
+++ src/test/java/steps/Steps.java
@@ -2,11 +2,8 @@ package steps;
 
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
-import io.cucumber.java.en.Then;
-import io.cucumber.java.en.When;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+
 
 public class Steps {
     static WebDriver driver;
